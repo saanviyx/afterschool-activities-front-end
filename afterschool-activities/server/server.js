14b212e7c7585e91b78410f3f25d7c7cf1605c71
Saanviya Sunil <saanviya@techunicorn.com>
2024-10-29 17:31:41 +0400

    feat: Added image for each lesson and set corresponding styles

--- afterschool-activities/server/server.js
+++ afterschool-activities/server/server.js
@@ -8,21 +8,22 @@ const PORT = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const jsonData = [
-    { id: 1, title: "Coding", location: "Hendon", price: 2000, spaces: 5, rating: 2.5 },
-    { id: 2, title: "KungFu", location: "Mauritius", price: 2500, spaces: 5, rating: 3 },
-    { id: 3, title: "Karate", location: "Dubai", price: 2200, spaces: 5, rating: 2 },
-    { id: 4, title: "Art", location: "Mauritius", price: 1900, spaces: 5, rating: 2.5},
-    { id: 5, title: "Robotics", location: "Hendon", price: 2300, spaces: 5, rating: 2.5},
-    { id: 6, title: "Tennis", location: "Dubai", price: 2000, spaces: 5, rating: 3},
-    { id: 7, title: "Guitar", location: "Dubai", price: 2100, spaces: 5, rating: 2.5 },
-    { id: 8, title: "Piano", location: "Hendon", price: 2090, spaces: 5, rating: 2},
-    { id: 9, title: "Drama", location: "Mauritius", price: 2100, spaces: 5, rating: 2},
-    { id: 10, title: "Photography", location: "Hendon", price: 2000, spaces: 5, rating: 3}
-];
-
-// Serve static files from the client directory
 app.use(express.static(path.join(__dirname, '../client')));
+
+app.use('/images', express.static(path.join(__dirname, '../client/images')));
+
+const jsonData = [
+    { id: 1, title: "Coding", location: "Hendon", price: 2000, spaces: 5, rating: 2.5, image: "/images/coding.jpg" },
+    { id: 2, title: "KungFu", location: "Mauritius", price: 2500, spaces: 5, rating: 3, image: "/images/kungfu.jpg" },
+    { id: 3, title: "Karate", location: "Dubai", price: 2200, spaces: 5, rating: 2, image: "/images/karate.jpg" },
+    { id: 4, title: "Art", location: "Mauritius", price: 1900, spaces: 5, rating: 2.5, image: "/images/art.jpg" },
+    { id: 5, title: "Robotics", location: "Hendon", price: 2300, spaces: 5, rating: 2.5, image: "/images/robotics.jpg" },
+    { id: 6, title: "Tennis", location: "Dubai", price: 2000, spaces: 5, rating: 3, image: "/images/tennis.jpg"},
+    { id: 7, title: "Guitar", location: "Dubai", price: 2100, spaces: 5, rating: 2.5, image: "/images/guitar.jpg" },
+    { id: 8, title: "Piano", location: "Hendon", price: 2090, spaces: 5, rating: 2, image: "/images/piano.jpg" },
+    { id: 9, title: "Drama", location: "Mauritius", price: 2100, spaces: 5, rating: 2, image: "/images/drama.jpg"},
+    { id: 10, title: "Photography", location: "Hendon", price: 2000, spaces: 5, rating: 3, image: "/images/photography.jpg" }
+];
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'));
